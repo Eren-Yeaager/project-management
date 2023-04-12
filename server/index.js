2010a90546e3c69d6db10059f74acb2177ba48d5
Marpureddy Saswat <saswatmarpureddy@gmail.com>
2023-04-12 11:56:39 +0530

    Project & Client Queries

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
+const connectDB = require("./config/db");
+const colors = require("colors");
 const schema = require("./schema/schema");
 const port = process.env.PORT || 5000;
 const app = express();
+connectDB();
 app.use(
   "/graphql",
   graphqlHTTP({
